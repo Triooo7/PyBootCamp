f2d259319864f60abfb95a0fd9bc3890b625c381
Krishnam Raju <raju.krishnam@volvocars.com>
2021-01-08 10:27:16 +0100

    Day-1
    Making the BandNameGenerator Project
    
    PY-8/1/2021.

---
+++ Day-1(Printing, Commenting, Debugging, String manipulation and variables)/BandNameGenerator.py
@@ -0,0 +1,9 @@
+print("Welcome to Band Name Generator !!!")
+city = input("What City are you from ? \n")
+print(city)
+favGame = input("What is the name of your fav game ? \n")
+print(favGame)
+bandName = city + favGame
+print("Your band name could be : " + bandName)
+
+
